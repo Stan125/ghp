2cf9a5386d60348c15e9b3451d702bdd79c88546
Stan125 <stanislaus@stadlmann.cm>
2017-07-18 15:31:32 +0200

    Add acc() to get all possible cov combinations

--- R/ghp.R
+++ R/ghp.R
@@ -1,12 +1,42 @@
+#' Function to get all possible combinations for a set of covariates
+#'
+#' @importFrom gtools combinations
+#' @export
+
+acc <- function(indep) {
+  combs <- function(k) {
+    ## Combinations
+    combs <- as.list(1:k)
+    combs <- lapply(combs, FUN = function(x)
+      return(matrix(0, nrow = n_combs(k, x), ncol = k)))
+    for (i in 1:length(combs)) {
+      temp_comb <- combinations(k, i)
+      dtc <- dim(temp_comb)
+      combs[[i]][1:dtc[1], 1:dtc[2]] <- temp_comb
+    }
+    combs <- do.call(rbind, combs)
+
+    ## Binary combinations
+    indices <- cbind(1:nrow(combs), as.vector(combs))
+    indices <- indices[indices[, 2] > 0, ]
+    binary <- matrix(0, nrow = nrow(combs), ncol = ncol(combs))
+    binary[indices] <- 1
+
+    return(binary)
+  }
+}
+
+#' Internal function: Calculate number of combinations
+#'
+n_combs <- function(n, r)
+  return(factorial(n) / (factorial(n - r) * factorial(r)))
+
 #' Function to do hierarchical partitioning
 #'
-#' import dplyr
-#' importFrom combinat permn
-#' importFrom Matrix bdiag
-
-########################
-##### ghp function #####
-########################
+#' @import dplyr
+#' @importFrom combinat permn
+#' @importFrom Matrix bdiag
+#' @export
 
 ghp <- function(dep, indep, barplot = FALSE, gof = "r.squared",
                 model_fun = lm, ...) {
